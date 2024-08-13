d7f7b650869b67c0fe5e121e336a6ea6c124c6c3
gabriellsux <gabrielpereiramartins49@gmail.com>
2024-08-12 23:15:53 -0300

    Estruturando o JS do projeto

--- calculadora.js
+++ calculadora.js
@@ -11,8 +11,8 @@ let numeroAnterior;
 const operacaoPendente = () => operador !== undefined;
 
 const calcular = () => {
-    if(operadorPendente()) {
-        const numeroAtual = parseFloat(display.textContent.replace(',','').replace(',','.'));
+    if (operacaoPendente()) {
+        const numeroAtual = parseFloat(display.textContent.replace('.','').replace(',', '.'));
         novoNumero = true;
         const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`);
         atualizarDisplay(resultado);
@@ -20,7 +20,7 @@ const calcular = () => {
 };
 
 const atualizarDisplay = (texto) => {
-    if(novoNumero) {
+    if (novoNumero) {
         display.textContent = texto.toLocaleString('BR');
         novoNumero = false;
     } else {
@@ -33,25 +33,27 @@ const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
 numeros.forEach((numero) => numero.addEventListener('click', inserirNumero));
 
 const selecionarOperador = (evento) => {
-    if(!novoNumero) {
+    if (!novoNumero) {
         calcular();
         novoNumero = true;
         operador = evento.target.textContent;
-        numeroAnterior = parseFloat(display.textContent.replace(',','').replace(',','.'));
+        numeroAnterior = parseFloat(display.textContent.replace('.','').replace(',', '.'));
     }
 };
-
-operador.forEach((operador) => operador.addEventListener('click', selecionarOperador));
+operadores.forEach((operador) =>
+    operador.addEventListener('click', selecionarOperador)
+);
 
 const ativarIgual = () => {
     calcular();
     operador = undefined;
 };
-
 document.getElementById('igual').addEventListener('click', ativarIgual);
 
 const limparDisplay = () => (display.textContent = '');
-document.getElementById('limparDisplay').addEventListener('click', limparDisplay);
+document
+    .getElementById('limparDisplay')
+    .addEventListener('click', limparDisplay);
 
 const limparCalculo = () => {
     limparDisplay();
@@ -59,7 +61,61 @@ const limparCalculo = () => {
     novoNumero = true;
     numeroAnterior = undefined;
 };
-document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+document
+    .getElementById('limparCalculo')
+    .addEventListener('click', limparCalculo);
 
-const removerUltimoNumero = () => (display.textContent = display.textContent.slice(0, -1));
-document.getElementById('backspace').addEventListener('click', removerUltimoNumero);
+const removerUltimoNumero = () =>
+    (display.textContent = display.textContent.slice(0, -1));
+document
+    .getElementById('backspace')
+    .addEventListener('click', removerUltimoNumero);
+
+const inverterSinal = () => {
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1);
+};
+document.getElementById('inverter').addEventListener('click', inverterSinal);
+
+const existeDecimal = () => display.textContent.indexOf(',') !== -1;
+const existeValor = () => display.textContent.length > 0;
+const inserirDecimal = () => {
+    if (!existeDecimal()) {
+        if (novoNumero) {
+            atualizarDisplay('0,');
+        } else {
+            atualizarDisplay(',');
+        }
+    }
+};
+document.getElementById('decimal').addEventListener('click', inserirDecimal);
+
+const mapaTeclado = {
+    0: 'tecla0',
+    1: 'tecla1',
+    2: 'tecla2',
+    3: 'tecla3',
+    4: 'tecla4',
+    5: 'tecla5',
+    6: 'tecla6',
+    7: 'tecla7',
+    8: 'tecla8',
+    9: 'tecla9',
+    '/': 'operadorDividir',
+    '*': 'operadorMultiplicar',
+    '-': 'operadorSubtrair',
+    '+': 'operadorAdicionar',
+    '=': 'igual',
+    Enter: 'igual',
+    Backspace: 'backspace',
+    c: 'limparDisplay',
+    Escape: 'limparCalculo',
+    ',': 'decimal',
+};
+
+const mapearTeclado = (evento) => {
+    const tecla = evento.key;
+    const teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1;
+    if (teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click();
+};
+document.addEventListener('keydown', mapearTeclado);
